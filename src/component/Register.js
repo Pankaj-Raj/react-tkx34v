b14e35b3a0274e9f743657395cc6e8827cf67bd2
Pankaj-Raj <pankajrajput15694@gmail.com>
2022-05-21 19:52:01 +0530

    1 file modified

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 export default class Register extends React.Component{
 render(){
 return(
 <div>
-
+<Link to="/">Nav</Link>
   <h1>jhgjhghjg</h1>
 </div>
 
